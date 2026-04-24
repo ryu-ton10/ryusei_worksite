d2627e5d147348895a91e0d0e88553ec9df3f7bf
ryu-ton10 <kanontonto1010@gmail.com>
2026-04-24 19:58:07 +0900

    create portfolio

---
+++ app/portfolio/component/project.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import './project.scss';
+import type { StaticImageData } from "next/image"
+
+export function Project(
+  {
+    title,
+    description,
+    imageSrc,
+    link
+  }
+  :
+  {
+    title: string,
+    description: string,
+    imageSrc: StaticImageData,
+    link: string
+  }
+) {
+  return (
+    <div className="portfolio__project">
+      <div className="portfolio__project-title">
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+      <div className="portfolio__project-image-container">
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          <Image
+            fill
+            src={imageSrc}
+            alt={title}
+            className="portfolio__project-image"
+            style={{ objectFit: 'contain' }}
+          />
+        </a>
+      </div>
+    </div>
+  )
+}
