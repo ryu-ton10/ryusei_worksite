75a72607542c47801e85624d008112b6b303c30e
ryu-ton10 <kanontonto1010@gmail.com>
2026-04-25 11:46:53 +0900

    add contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,13 @@
+import './page.scss';
+
+export default function Contact() {
+  return (
+    <div className="contact">
+      <h1>Contact</h1>
+      <div className="contact__content">
+        <p>お問い合わせは以下のメールアドレスまでお願いいたします。</p>
+        <a href="mailto:ryusakine0507@gmail.com">ryusakine0507@gmail.com</a>
+      </div>
+    </div>
+  );
+}
