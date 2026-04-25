ab26eaede35282cf89d101cf6312c071a5c1ea30
ryu-ton10 <kanontonto1010@gmail.com>
2026-04-25 15:44:57 +0900

    design revision

--- app/component/footer/footer.tsx
+++ app/component/footer/footer.tsx
@@ -3,7 +3,9 @@ import './footer.scss';
 export function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; 2026 Ryusei Nomi. All rights reserved.</p>
+      <a href="https://x.com/skn_ton10_v1" target="_blank" rel="noopener noreferrer">
+        <p>&copy; 2026 Ryusei Nomi. All rights reserved.</p>
+      </a>
     </footer>
   );
 }
